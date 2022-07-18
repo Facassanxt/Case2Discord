ae2b344dd7e6b8320b591ded0925e1051a15554a
Facassanxt <asteblo@gmail.com>
2022-07-19 01:43:45 +0300

    Fix

--- main.py
+++ main.py
@@ -32,79 +32,80 @@ class MyHandler(FileSystemEventHandler):
 
     def on_any_event(self, event):
         drop_path = os.path.join(DIRECTORY,FILE)
-        if event.src_path == drop_path:
+        if event.src_path == drop_path and event.event_type == 'modified':
             print(event)
             with open(drop_path, encoding = 'utf-8') as f:
-                last_line = f.readlines()[-1]
-            os.remove(drop_path)
-            with open(drop_path, mode='a'): pass
-            result = re.findall(r'^L ([0-9\/]*) - ([0-9]{2}:[0-9]{2}:[0-9]{2}).*Игроку (.*)<[0-9]{1,5}><(.*)><.*> выпало \[?([0-9]*)', last_line)[0]
-            user_steamid = int(result[3].split(":")[2])*2+int(result[3].split(":")[1])+76561197960265728
-            api_user = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key={}&steamids={}".format(STEAM_API_DEV_KEY,user_steamid)
-            try:
-                rq = requests.get(api_user).json()
-                user_avatar = rq['response']['players'][0]['avatarfull']
-            except:
-                user_avatar = None
+                queue = f.read()
+            for line in queue.splitlines():
+                os.remove(drop_path)
+                with open(drop_path, mode='a'): pass
+                result = re.findall(r'^L ([0-9\/]*) - ([0-9]{2}:[0-9]{2}:[0-9]{2}).*Игроку (.*)<[0-9]{1,5}><(.*)><.*> выпало \[?([0-9]*)', line)[0]
+                user_steamid = int(result[3].split(":")[2])*2+int(result[3].split(":")[1])+76561197960265728
+                api_user = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key={}&steamids={}".format(STEAM_API_DEV_KEY,user_steamid)
+                try:
+                    rq = requests.get(api_user).json()
+                    user_avatar = rq['response']['players'][0]['avatarfull']
+                except:
+                    user_avatar = None
 
-            with open("Case.json", encoding = 'utf-8') as f:
-                jcase = json.load(f)
+                with open("Case.json", encoding = 'utf-8') as f:
+                    jcase = json.load(f)
 
-            num = result[4]
-            data_time = result[0] + " " + result[1]
-            user_login = result[2]
-            user_profile = "https://steamcommunity.com/profiles/" + str(user_steamid)
-            try:
-                case_name = jcase[num]["eng_case_name"]
-                market_case = jcase[num]["eng_case_name"]
-                case_url = jcase[num]["image_url"]
-                text_price = "Цена: `{}`".format(Price_Cases[num])
-                market_url = ("https://steamcommunity.com/market/listings/730/" + market_case).replace(" ","%20")
-            except:
-                case_name = "Неизвестный кейс"
-                market_url = None
-                case_url = None
-                text_price = None
-            with open("Discord.json", encoding = 'utf-8') as f:
-                discord = json.load(f)
-            for id in discord:
-                if id != "_comment":
-                    if user_steamid in discord[id]:
-                        if text_price != None:
-                            text_price += " <@{}>".format(id)
-                        else:
-                            text_price = "<@{}>".format(id)
-            Random_color = ''.join([random.choice('0123456789ABCDEF') for i in range(6)])
-            print(case_name,case_url,market_url,text_price)
-            data = {
-                "username" : "Drops_Cases",
-                "embeds": [
-                    {
-                    "title": case_name,
-                    "description": text_price,
-                    "url": market_url,
-                    "color": int(Random_color,16),
-                    "fields": [
+                num = result[4]
+                data_time = result[0] + " " + result[1]
+                user_login = result[2]
+                user_profile = "https://steamcommunity.com/profiles/" + str(user_steamid)
+                try:
+                    case_name = jcase[num]["eng_case_name"]
+                    market_case = jcase[num]["eng_case_name"]
+                    case_url = jcase[num]["image_url"]
+                    text_price = "Цена: `{}`".format(Price_Cases[num])
+                    market_url = ("https://steamcommunity.com/market/listings/730/" + market_case).replace(" ","%20")
+                except:
+                    case_name = "Неизвестный кейс"
+                    market_url = None
+                    case_url = None
+                    text_price = None
+                with open("Discord.json", encoding = 'utf-8') as f:
+                    discord = json.load(f)
+                for id in discord:
+                    if id != "_comment":
+                        if user_steamid in discord[id]:
+                            if text_price != None:
+                                text_price += " <@{}>".format(id)
+                            else:
+                                text_price = "<@{}>".format(id)
+                Random_color = ''.join([random.choice('0123456789ABCDEF') for i in range(6)])
+                print(case_name,case_url,market_url,text_price)
+                data = {
+                    "username" : "Drops_Cases",
+                    "embeds": [
                         {
-                        "name": "ᅠ ᅠ",
-                        "value": "[`Профиль`]({}) [`Инвентарь`]({}) [`Маркет`]({})".format(user_profile,user_profile+"/inventory/#730",market_url) \
-                        +"\n\n :hourglass_flowing_sand:" + data_time + ":hourglass:",
+                        "title": case_name,
+                        "description": text_price,
+                        "url": market_url,
+                        "color": int(Random_color,16),
+                        "fields": [
+                            {
+                            "name": "ᅠ ᅠ",
+                            "value": "[`Профиль`]({}) [`Инвентарь`]({}) [`Маркет`]({})".format(user_profile,user_profile+"/inventory/#730",market_url) \
+                            +"\n\n :hourglass_flowing_sand:" + data_time + ":hourglass:",
+                            }
+                        ],
+                        "author": {
+                            "name": user_login,
+                            "url": user_profile,
+                            "icon_url": user_avatar
+                        },
+                        "thumbnail": {
+                            "url": case_url
+                        }
                         }
                     ],
-                    "author": {
-                        "name": user_login,
-                        "url": user_profile,
-                        "icon_url": user_avatar
-                    },
-                    "thumbnail": {
-                        "url": case_url
+                    "attachments": []
                     }
-                    }
-                ],
-                "attachments": []
-                }
-            resp = requests.post(DISCORDWEBHOOK, json=data)
-            print(resp)
+                resp = requests.post(DISCORDWEBHOOK, json=data)
+                print(resp)
 
 Price_Cases = {}
 def Price_parser():
